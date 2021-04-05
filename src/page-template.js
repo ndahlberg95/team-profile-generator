2982688ffc4463b774f7ab7e7150f1e65f6544bd
Nora Dahlberg <ndahlberg95@gmail.com>
2021-04-04 19:19:01 -0600

    fix style link

--- src/page-template.js
+++ src/page-template.js
@@ -1,7 +1,7 @@
 module.exports = templateData => {
-    console.log(templateData);
-  
-    return `
+  console.log(templateData);
+
+  return `
     <!DOCTYPE html>
     <html lang="en">
   
@@ -9,19 +9,35 @@ module.exports = templateData => {
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <link rel="stylesheet" href="./dist/style.css">
+      <link rel="stylesheet" href="style.css">
       <title>Team Profile</title>
     </head>
   
     <body>
 
-      <div id="header">
-      <h1>'My Team'</h1>
+      <div class="header">
+      <h1>My Team</h1>
       </div>
       
-      <div class="employees"></div>
+      <div class="employees">
+        
+        <div id="manager">
+          <div>
+            <h2></h2>
+            <h2>Manager</h2>
+          </div>
+
+          <div>
+            <h3>ID:</h3>
+            <h3>Email:</h3>
+            <h3>Office Number:<h3>
+          </div>
+
+        </div>
+      
+      </div>
 
     </body>
     </html>
     `;
-  };
+};
